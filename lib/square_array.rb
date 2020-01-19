276b0035fac663c4bf0c401694475904cd44143a
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-19 18:31:20 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,9 +1,9 @@
 def square_array(array)
   counter = 0
 
-  while array[counter] do
+  while array[counter] {
     array[counter] = array[counter] ** 2
     counter += 1
-    end
+  }
     array
 end
